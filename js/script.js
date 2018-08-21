d8964a1a53aec45b32b705f5333a2f8bb16eb497
Tomasz Lektorowski <lektor1993@mat.umk.pl>
2018-08-21 12:40:01 +0200

    bugfixes
    -changed font to more user-friend
    -changed rounding edges from 25px to 5px
    - changed semantics in gallery, now every person data is separate <p> tag
    -chaged font size to px+ media querries
    -changed thumbnails visuals, removed unwanted text decoration effect
    - changed hamburger icon to simple label "menu"
    - banners are now located in footer on 4 main pages
    -removed styling by id

--- js/script.js
+++ js/script.js
@@ -1,3 +1,9 @@
 $(document).ready(function () {
     $("a.grouped_elements").fancybox();
 });
+
+$(document).ready(function(){
+	$('#nav-icon1,#nav-icon2,#nav-icon3,#nav-icon4').click(function(){
+		$(this).toggleClass('open');
+	});
+});
